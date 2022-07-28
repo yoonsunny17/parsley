ce9b50764c900296d830dda5d4bb7b4980af35cb
마유선 <jasminema@naver.com>
2022-07-28 16:45:12 +0900

    [S07P12A604-117] feat: add user repository

--- backend/src/main/java/com/ssafy/db/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/db/repository/UserRepository.java
@@ -1,12 +1,29 @@
 package com.ssafy.db.repository;
 
+import com.ssafy.db.entity.Auth;
+import com.ssafy.db.entity.User;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Map;
 
+/**
+ * 사용자 관련 Repository
+ */
 @Repository
 public class UserRepository {
-    public Map<Object, Object> findUserByEmail(String email) {
-        return null;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(User user) {
+        em.persist(user);
     }
+
+    public User findByUserId(Long userId){
+        return em.find(User.class, userId);
+    }
+
+
 }
