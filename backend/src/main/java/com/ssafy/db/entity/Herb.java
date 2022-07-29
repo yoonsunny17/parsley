4bfe6c5b14044431fa3dca4ae063278f18fa3053
마유선 <jasminema@naver.com>
2022-07-29 18:17:31 +0900

    [S07P12A604-167] feat: add herb entity

---
+++ backend/src/main/java/com/ssafy/db/entity/Herb.java
@@ -0,0 +1,34 @@
+package com.ssafy.db.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * 사용자가 심은 작물 모델 정의.
+ */
+
+@Getter
+@Setter
+@Entity
+public class Herb {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "herb_id")
+    private Long id;
+
+    private int position;
+    private boolean isCompleted;
+    private int growthTime;
+    private LocalDateTime startDate;
+
+    @Embedded
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
