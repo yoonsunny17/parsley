bff2bc17e8bdeb786a867fded81fedc77b6e8ac0
YunYoug <jeongyy98>
2022-07-29 18:09:18 +0900

    [S07P12A604-169] feat: add sley entity

---
+++ backend/src/main/java/com/ssafy/db/entity/Sley.java
@@ -0,0 +1,27 @@
+package com.ssafy.db.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Sley {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "sley_id")
+    private Long id;
+
+    private long price;
+    private LocalDateTime date;
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
