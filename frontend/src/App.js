e5a14acf08a398578ab44690ab83a48b165b6510
Haru-arp <vr1997@naver.com>
2022-08-01 17:11:55 +0900

    [S07P12A604-196] feat: add studyroom page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import StudyRoom from "./components/pages/StudyRoom";
 
 function App() {
   return (
     <div className="App container mx-auto px-5">
       <BrowserRouter>
         <Routes>
+          <Route path="room" element={<StudyRoom />} />
         </Routes>
       </BrowserRouter>
     </div>
