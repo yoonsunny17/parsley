88f57ac52708d53c265509e58376f1422ff6db13
Haru-arp <vr1997@naver.com>
2022-08-02 16:45:34 +0900

    [S07P12A604-136] feat: add login logout sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/pages/Home'
+import Drawer from "./UI/molecules/Drawer";
+
 function App() {
   return (
-    <div className="App container mx-auto px-5">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} /> 
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <Drawer>
+      <div className="App container mx-auto px-5 drawer drawer-end">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} /> 
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Drawer>
   );
 }
 
