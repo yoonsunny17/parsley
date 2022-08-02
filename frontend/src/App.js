1ae170e02128fc99c4f7d8bfe280cd5d79c001cd
Haru-arp <vr1997@naver.com>
2022-08-02 14:04:02 +0900

    [S07P12A604-153] feat: add home route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Home from './components/pages/Home'
 function App() {
   return (
     <div className="App container mx-auto px-5">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} /> 
         </Routes>
       </BrowserRouter>
     </div>
