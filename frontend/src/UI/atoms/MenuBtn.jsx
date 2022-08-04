f4f1d50ea5cce1ce4faf212a75003400b593aa1b
yoonsunny17 <jyoonsun0217@gmail.com>
2022-08-04 11:37:11 +0900

    fix: relocated menubtn folder from molecules to atoms

---
+++ frontend/src/UI/atoms/MenuBtn.jsx
@@ -0,0 +1,12 @@
+function MenuBtn() {
+  return (
+    <label
+      htmlFor="my-drawer-4"
+      className="drawer-button text-4xl cursor-pointer"
+    >
+      <i className="bx bx-menu"></i>
+    </label>
+  );
+}
+
+export default MenuBtn;
