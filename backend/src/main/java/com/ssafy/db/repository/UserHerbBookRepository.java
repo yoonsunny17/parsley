2dfaf4c8243e1718d6d5f751aca15341111194f4
마유선 <jasminema@naver.com>
2022-08-04 17:38:57 +0900

    [S07P12A604-171] fix: update user herb book repository - add method

--- backend/src/main/java/com/ssafy/db/repository/UserHerbBookRepository.java
+++ backend/src/main/java/com/ssafy/db/repository/UserHerbBookRepository.java
@@ -1,11 +1,12 @@
 package com.ssafy.db.repository;
 
+import com.ssafy.db.entity.User;
 import com.ssafy.db.entity.UserHerbBook;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 /**
  * 사용자 도감 Repository
@@ -18,5 +19,11 @@ public class UserHerbBookRepository {
 
     public void save(UserHerbBook userHerbBook){
         em.persist(userHerbBook);
+    }
+
+    public List<UserHerbBook> findByUser(User user){
+        return em.createQuery("select u from UserHerbBook u where u.user = :user", UserHerbBook.class)
+                .setParameter("user", user)
+                .getResultList();
     }
 }
