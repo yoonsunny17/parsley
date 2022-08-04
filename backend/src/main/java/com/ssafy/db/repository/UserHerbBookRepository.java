41ad321f39d7683bd49c4a23058929d899f52177
마유선 <jasminema@naver.com>
2022-08-05 02:41:04 +0900

    [S07P12A604-171] fix: update user herb book repository - add method

--- backend/src/main/java/com/ssafy/db/repository/UserHerbBookRepository.java
+++ backend/src/main/java/com/ssafy/db/repository/UserHerbBookRepository.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Tuple;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,5 +31,22 @@ public class UserHerbBookRepository {
         return em.createQuery("select u from UserHerbBook u where u.user = :user", UserHerbBook.class)
                 .setParameter("user", user)
                 .getResultList();
+    }
+
+    public List<Tuple> findByUserAndGroupBy(User user){
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Tuple> q = cb.createTupleQuery();
+        Root<UserHerbBook> c = q.from(UserHerbBook.class);
+        q.multiselect(c.get("herbBook"), cb.count(c));
+        q.where(cb.equal(c.get("user"), user));
+        q.groupBy(c.get("herbBook"));
+
+        TypedQuery<Tuple> t = em.createQuery(q);
+        List<Tuple> resultList = t.getResultList();
+
+//        for(Tuple tuple : resultList){
+//            System.out.println(tuple.get(0) + "   " + tuple.get(1));
+//        }
+        return resultList;
     }
 }
