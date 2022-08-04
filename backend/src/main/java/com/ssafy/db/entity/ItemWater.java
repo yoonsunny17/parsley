a84a1af50c0248234eaf76cb692365db00b3e4cc
마유선 <jasminema@naver.com>
2022-08-04 17:27:30 +0900

    [S07P12A604-167] fix: update item water entity - change to entity

--- backend/src/main/java/com/ssafy/db/entity/ItemWater.java
+++ backend/src/main/java/com/ssafy/db/entity/ItemWater.java
@@ -3,13 +3,16 @@ package com.ssafy.db.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Embeddable;
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @DiscriminatorValue("W")
 @Getter @Setter
-@Embeddable
-public class ItemWater extends Item{
+@Entity
+public class ItemWater extends ItemAbstract {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_water_id")
+    private Integer id;
+
     private Integer timeRate;
 }
