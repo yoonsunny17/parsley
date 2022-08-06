38dd293dc1f82bacf36d45638f099b6605eb3b41
yoonsunny17 <jyoonsun0217@gmail.com>
2022-08-06 17:12:54 +0900

    [S07P12A604-202] test: unstable OpenVidu codes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AuthLogin from "./UI/pages/AuthLogin";
-import CreateStudyRoom from "./UI/pages/CreateStudyRoom";
-import EnterStudyRoom from "./UI/pages/EnterStudyRoom";
-import Home from "./UI/pages/Home";
-import StudyRoomOV from "./UI/pages/StudyRoomOV";
+import AuthLogin from "./components/pages/AuthLogin";
+import CreateStudyRoom from "./components/pages/CreateStudyRoom";
+import Home from "./components/pages/Home";
+// import StudyRoomOV from "./components/pages/StudyRoomOV";
 // import StudyRoom from "./components/pages/StudyRoom";
 import Drawer from "./UI/organisms/Drawer";
-import SearchStudyRooms from "./UI/pages/SearchStudyRooms";
+// import FingerStudyRoom from "./OpenVidu/FingerStudyRoom";
+import StudySession from "./OpenVidu/StudySession";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Drawer>
-                <div className="App container mx-auto px-5 mb-10">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route
-                            path="/room/create"
-                            element={<CreateStudyRoom />}
-                        />
-                        <Route path="/user/login" element={<AuthLogin />} />
-                        <Route path="/room" element={<EnterStudyRoom />} />
-                        <Route path="/search" element={<SearchStudyRooms />} />
-                    </Routes>
-                </div>
-            </Drawer>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Drawer>
+        <div className="App container mx-auto px-5 drawer drawer-end">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/room/create" element={<CreateStudyRoom />} />
+            <Route path="/user/login" element={<AuthLogin />} />
+            {/* <Route path="/studyroom" element={<StudyRoom />} /> */}
+            {/* <Route path="/studyroom" element={<FingerStudyRoom />} /> */}
+            <Route path="/room" element={<StudySession />} />
+          </Routes>
+        </div>
+      </Drawer>
+    </BrowserRouter>
+  );
 }
 
 export default App;
