2e18b6ab77b44cbd1c7b06c65b93771bc5d31fcf
yoonsunny17 <jyoonsun0217@gmail.com>
2022-08-06 17:06:54 +0900

    [S07P12A604-202] feat: add OpenVidu layout codes

--- frontend/src/OpenVidu/UserVideoComponent.js
+++ frontend/src/OpenVidu/UserVideoComponent.js
@@ -1,62 +1,22 @@
 import React, { Component } from "react";
 import OpenViduVideoComponent from "./OvVideo";
+import "./UserVideo.css";
 
-class UserVideoComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // mic, video status
-      audioState: false,
-      videoState: false,
-      isSpeaking: false,
-    };
-  }
+export default class UserVideoComponent extends Component {
   getNicknameTag() {
     // Gets the nickName of the user
     return JSON.parse(this.props.streamManager.stream.connection.data)
       .clientData;
   }
 
-  forceMicControl(connection) {
-    let remoteUsers = this.props.subscribers;
-    remoteUsers.forEach((user) => {
-      if (user.stream.connection.connectionId === connection.connectionId) {
-        user.stream.audioActive = !user.stream.audioActive;
-
-        this.props.storeSession.signal({
-          data: user.stream.audioActive,
-          to: [connection],
-          type: "audio",
-        });
-      }
-    });
-    this.props.doUpdateSubscriber(remoteUsers);
-  }
-
-  forceVideoControl(connection) {
-    let remoteUsers = this.props.subscribers;
-    remoteUsers.forEach((user) => {
-      if (user.stream.connection.connectionId === connection.connectionId) {
-        user.stream.videoActive = !user.stream.videoActive;
-
-        this.props.storeSession.signal({
-          data: user.stream.videoActive,
-          to: [connection],
-          type: "video",
-        });
-      }
-    });
-    this.props.doUpdateSubscriber(remoteUsers);
-  }
-
   render() {
     return (
       <div>
         {this.props.streamManager !== undefined ? (
-          <div className="w-full h-auto">
+          <div className="streamcomponent">
             <OpenViduVideoComponent streamManager={this.props.streamManager} />
-            <div className="absolute bg-font4 px-[5px] text-font1 font-bold rounded-br-[4px]">
-              {this.getNicknameTag()}
+            <div>
+              <p>{this.getNicknameTag()}</p>
             </div>
           </div>
         ) : null}
@@ -64,5 +24,3 @@ class UserVideoComponent extends Component {
     );
   }
 }
-
-export default UserVideoComponent;
