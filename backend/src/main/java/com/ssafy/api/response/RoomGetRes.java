4db061fa9f450e51875d7004c4b746948fbfb84e
마유선 <jasminema@naver.com>
2022-08-07 11:51:18 +0900

    [S07P12A604-250] refactor: change to data annotation

--- backend/src/main/java/com/ssafy/api/response/RoomGetRes.java
+++ backend/src/main/java/com/ssafy/api/response/RoomGetRes.java
@@ -4,11 +4,11 @@ import com.ssafy.common.model.response.BaseResponseBody;
 import com.ssafy.db.entity.Room;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 @ApiModel("RoomGetResponse")
 public class RoomGetRes extends BaseResponseBody {
     @ApiModelProperty(name = "방 정보")
