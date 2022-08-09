74ed8fcf1ae7428ca6cf979a0064ff74df5cb649
Haru-arp <vr1997@naver.com>
2022-08-09 14:55:35 +0900

    [S07P12A604-312] feat: add weekStudy component

--- frontend/src/UI/organisms/DailyStudy.jsx
+++ frontend/src/UI/organisms/DailyStudy.jsx
@@ -1,8 +1,80 @@
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
+
+
+const data = [
+  {
+    name: "월",
+    hour: 4,
+    amt: 2400
+  },
+  {
+    name: "화",
+    hour: 1.3,
+    amt: 2210
+  },
+  {
+    name: "수",
+    hour: 12,
+    amt: 2290
+  },
+  {
+    name: "목",
+    hour: 7,
+    amt: 2000
+  },
+  {
+    name: "금",
+    hour: 8,
+    amt: 2181
+  },
+  {
+    name: "토",
+    hour: 9,
+    amt: 2500
+  },
+  {
+    name: "일",
+    hour: 9,
+    amt: 2100
+  }
+];
+
 function DailyStudy() {
   return (
-    <div className="shadow-sm rounded-[10px] px-4 py-3 w-full mb-2 lg:w-2/3 lg:mb-0 border-[2px] border-sub1">
-      <div className="text-base font-semibold">요일별 공부량</div>
+    <div className="flex flex-col shadow-sm rounded-[10px] px-4 py-3 w-full mb-2 lg:w-2/3 lg:mb-0 border-[2px] border-sub1">
+      <div className="font-semibold text-[20px]">요일별 공부량</div>
+      <div className="font-normal text-[24px]"><span className="font-bold">{data[2].name}요일</span>에 가장 열심히 공부했네요!</div>
+      <div className="mt-[30px] text-center">
+        <BarChart
+          width={900}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="hour" fill="#6366F1" />
+        </BarChart>
+      </div>
+    
     </div>
+    
   );
 }
 
--- frontend/src/UI/organisms/TodayStudyTime.jsx
+++ frontend/src/UI/organisms/TodayStudyTime.jsx
@@ -1,17 +1,17 @@
 function TodayStudyTime() {
 	const FULL_MINUTES = 1440;
-  const NOW_TIME = 1117;
+  const NOW_TIME = 50;
 
 	const BOX_RENDER_LIST = new Array(FULL_MINUTES / 10)
 		.fill(null)
-		.map((_, index) => (index + 1) * 10);
+		.map((_, index) => (index) * 10);
 	const MINUTE = [];
 
 	while (BOX_RENDER_LIST.length) {
 		MINUTE.push(BOX_RENDER_LIST.splice(0, 6));
 	}
 
-
+  console.log(NOW_TIME)
 	return (
 		<div className="rounded-2xl shadow px-8 py-5 w-full md:w-2/3 p-[28px_30px]">
 			<div className="font-basic text-[24px] font-bold mb-[40px] ">오늘의 공부량</div>
@@ -31,8 +31,8 @@ function TodayStudyTime() {
             <div className="flex flex-col">
               {v.map((v) => (
                 <div className="w-[18px] h-[18px] rounded-[4px] border-[0.5px] border-font2 bg-[#D9D9D9] relative m-[4px] overflow-hidden">
-                  <div className={`absolute left-0 top-0 bg-sub1 w-full h-full opacity-[${NOW_TIME-v > 10 ? '100%' : NOW_TIME-v > 5 ? '50%' : '0%'}]`} />
-                </div>
+                  <div className={`absolute left-0 top-0 bg-sub1 w-full h-full ${NOW_TIME - v >= 10 ? 'opacity-100' : NOW_TIME-v >= 5 ? 'opacity-50' : 'opacity-0'}`} />
+                </div> 
               ))}
             </div>
           ))}
@@ -41,7 +41,7 @@ function TodayStudyTime() {
 		</div>
 	);
 }
-
+// ${NOW_TIME -v >= 10 ? 1 : NOW_TIME - v >= 5 ? 0.5: 0}
 export default TodayStudyTime;
 
 // <div className="w-16 h-16 rounded-md bg-gray-500 relative mb-4">
