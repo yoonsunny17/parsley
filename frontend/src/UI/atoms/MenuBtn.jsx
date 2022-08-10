bd162c50145ce6b9f36914689d97c63782f60cb7
KANCHOEUN <kancho1216@naver.com>
2022-08-10 13:27:11 +0900

    [S07P12A604-263] refactor: redundant code

--- frontend/src/UI/atoms/MenuBtn.jsx
+++
@@ -1,9 +0,0 @@
-function MenuBtn() {
-  return (
-        <label htmlFor="my-drawer-4" className="drawer-button text-4xl cursor-pointer">
-          <i className="bx bx-menu"></i>
-        </label>
-  )
-}
-
-export default MenuBtn
