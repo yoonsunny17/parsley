b37cd4ddf346a832a1e0a47860c15ea34eddb74d
yoonsunny17 <jyoonsun0217@gmail.com>
2022-08-11 11:55:17 +0900

    [S07P12A604-305] fix: resize avatar ring size

--- frontend/src/UI/atoms/HerbItemAvatar.jsx
+++ frontend/src/UI/atoms/HerbItemAvatar.jsx
@@ -1,7 +1,9 @@
+// 농장 상점 모달 페이지에서 우측의 구매 목록의 avatar
+
 function HerbItemAvatar({ imgUrl }) {
   return (
     <img
-      className="inline-block mt-3 mb-3 mr-3 h-12 w-12 rounded-full ring-2 ring-white"
+      className="inline-block mt-3 mb-3 mr-3 h-10 w-10 rounded-full ring-2 ring-sub1"
       src={imgUrl}
       alt="avatar"
     />
