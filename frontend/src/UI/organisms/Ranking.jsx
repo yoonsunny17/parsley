323dbe3c8c00cbc781af1d0afca9194cd5612ab0
Haru-arp <vr1997@naver.com>
2022-08-13 18:08:44 +0900

    [S07P12A604-314] fix: main ranking, noti cursor

--- frontend/src/UI/organisms/Ranking.jsx
+++ frontend/src/UI/organisms/Ranking.jsx
@@ -1,51 +1,56 @@
 import { useState } from "react";
 
 function Ranking() {
+    const [rank, setRank] = useState(10);
+    const [point, setPoint] = useState(10000);
 
-  const [rank, setRank] = useState(10)
-  const [point, setPoint] = useState(10000)
-
-  return (
-    <div className="bg-white rounded-3xl drop-shadow px-8 py-5 w-full md:w-[32%]">
-      <h3 className="text-xl font-bold text-center mb-4">
-        오늘의 농부왕
-      </h3>
-      <ul className="list-none">
-        <li className="flex items-center justify-around mb-8">
-          <i className='bx bx-medal text-[#D5A11E] text-[48px]'></i>
-          <div className="flex justify-center items-center w-[50px] h-[50px] rounded-[50%] bg-main">
-            <div className="w-[40px] h-[40px] rounded-[50%] bg-white"></div>
-          </div>
-          <span>슬리파슬리</span>
-          <span>{point}점</span>
-        </li>
-        <li className="flex items-center justify-around mb-8">
-          <i className='bx bx-medal text-[#A3A3A3] text-[48px]'></i>
-          <div className="flex justify-center items-center w-[50px] h-[50px] rounded-[50%] bg-main">
-            <div className="w-[40px] h-[40px] rounded-[50%] bg-white"></div>
-          </div>
-          <span>슬리파슬리</span>
-          <span>{point}점</span>
-        </li>
-        <li className="flex items-center justify-around mb-8">
-          <i className='bx bx-medal text-[#CD7F32] text-[48px]'></i>
-          <div className="flex justify-center items-center w-[50px] h-[50px] rounded-[50%] bg-main">
-            <div className="w-[40px] h-[40px] rounded-[50%] bg-white"></div>
-          </div>
-          <span>슬리파슬리</span>
-          <span>{point}점</span>
-        </li>
-        <li className="flex items-center justify-around mb-8 ">
-          {rank}등
-          <div className="flex justify-center items-center ml-[15px] w-[50px] h-[50px] rounded-[50%] bg-main">
-            <div className="w-[40px] h-[40px] rounded-[50%] bg-white"></div>
-          </div>
-          <span>슬리파슬리</span>
-          <span>{point}점</span>
-        </li>
-      </ul>
-    </div>
-  );
+    return (
+        <div className="bg-white rounded-3xl drop-shadow px-8 py-5 w-full md:w-[32%]">
+            <h3 className="text-xl font-bold text-center mb-4">오늘의 농부왕</h3>
+            <ul className="list-none">
+                <li className="flex items-center justify-around mb-8">
+                    <i className="bx bx-medal text-[#D5A11E] text-[48px]"></i>
+                    <div className="avartar">
+                        <div className="w-[55px]">
+                            <img className="rounded-full" src="https://placeimg.com/192/192/people" alt="" />
+                        </div>
+                    </div>
+                    <span>슬리파슬리</span>
+                    <span>{point}점</span>
+                </li>
+                <li className="flex items-center justify-around mb-8">
+                    <i className="bx bx-medal text-[#A3A3A3] text-[48px]"></i>
+                    <div className="avartar">
+                        <div className="w-[55px]">
+                            <img className="rounded-full" src="https://placeimg.com/192/192/people" alt="" />
+                        </div>
+                    </div>
+                    <span>슬리파슬리</span>
+                    <span>{point}점</span>
+                </li>
+                <li className="flex items-center justify-around mb-8">
+                    <i className="bx bx-medal text-[#CD7F32] text-[48px]"></i>
+                    <div className="avartar">
+                        <div className="w-[55px]">
+                            <img className="rounded-full" src="https://placeimg.com/192/192/people" alt="" />
+                        </div>
+                    </div>
+                    <span>슬리파슬리</span>
+                    <span>{point}점</span>
+                </li>
+                <li className="flex items-center justify-around mb-8 ">
+                    {rank}등
+                    <div className="avartar">
+                        <div className="w-[55px] ml-[15px]">
+                            <img className="rounded-full" src="https://placeimg.com/192/192/people" alt="" />
+                        </div>
+                    </div>
+                    <span>슬리파슬리</span>
+                    <span>{point}점</span>
+                </li>
+            </ul>
+        </div>
+    );
 }
 
 export default Ranking;
