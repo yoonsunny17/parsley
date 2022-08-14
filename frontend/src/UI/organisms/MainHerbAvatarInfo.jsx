1e65a40264f84347f91ccdffb415a9064028c179
yoonsunny17 <jyoonsun0217@gmail.com>
2022-08-14 14:56:04 +0900

    [S07P12A604-303] feat: update mainpage herb collections

---
+++ frontend/src/UI/organisms/MainHerbAvatarInfo.jsx
@@ -0,0 +1,53 @@
+// 메인 페이지에 있는 도감 컴포넌트의 아바타 atomic components
+// 농장 페이지랑 레이아웃이 달라서 반응형때문에 따로 만듦. 정보는 똑같음!
+import React from "react";
+import Swal from "sweetalert2";
+
+// FIXME: sweetalert로 description 보여주기
+function MainHerbAvatarInfo({
+  info: { name, imgUrl, grade, description, isOpened },
+}) {
+  return (
+    <div>
+      {isOpened ? (
+        <img
+          className="mx-[6px] my-1 inline-block h-10 w-10 rounded-full ring-2 ring-sub1 hover:scale-105 duration-200 cursor-pointer"
+          // className="cursor-pointer inline-block mt-3 mb-3 mr-3 h-12 w-12 rounded-full ring-2 ring-sub1"
+          src={imgUrl}
+          alt="avatar"
+          onClick={() => {
+            Swal.fire({
+              width: 350,
+              title: [name],
+              text: "[" + [grade] + "] " + [description],
+              imageUrl: [imgUrl], // 시간 되면 여기 사진 다른걸로 바꾸기
+              imageWidth: 50,
+              imageHeight: 50,
+              imageAlt: "Herb Collection Image",
+            });
+          }}
+        />
+      ) : (
+        <img
+          className="mx-[6px] my-1 inline-block h-10 w-10 rounded-full ring-2 ring-extra1"
+          // className="cursor-pointer inline-block mt-3 mb-3 mr-3 h-12 w-12 rounded-full ring-2 ring-extra1"
+          src="/herbs/isNotOpened.png"
+          alt="avatar"
+          onClick={() => {
+            Swal.fire({
+              width: 350,
+              icon: "error",
+              title: "Oops...",
+              text: "아직 획득하지 못했어요!",
+              footer: "슬리를 획득하여 허브 도감을 채워보세요:)",
+            });
+          }}
+        />
+      )}
+    </div>
+    // <div className="tooltip" data-tip={description}>
+    // </div>
+  );
+}
+
+export default MainHerbAvatarInfo;
---
+++ frontend/src/UI/organisms/MainPageHerbCollections.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import herbCollectionList from "../molecules/HerbCollectionList";
+// import FarmCollectionAvatarInfo from "./FarmCollectionAvatarInfo";
+import MainHerbAvatarInfo from "./MainHerbAvatarInfo";
+
+function Collections() {
+  const username = "유교보이";
+  const herbname = "프로틴 중독 파슬리";
+
+  return (
+    <div className="w-full mb-5 rounded-3xl bg-white drop-shadow px-6 py-5 lg:w-2/3 lg:mb-0">
+      <header className="flex justify-between items-center">
+        <h3 className="text-xl font-bold">{username} 님의 도감</h3>
+        <Link to="/farm">
+          <span className="rounded-full px-2 py-1 text-sm">내 농장</span>
+        </Link>
+      </header>
+      <div className="grid lg:grid-cols-largeCollections items-center justify-center">
+        <div className="flex flex-col items-center mt-5">
+          <img
+            className="rounded-full w-28 h-28"
+            src="https://images.unsplash.com/photo-1463320898484-cdee8141c787?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            alt="profileHerb"
+          />
+          <div className="my-3 font-semibold">{herbname}</div>
+        </div>
+        <div className="px-4 pt-8 md:px-10 w-full grid grid-cols-5 md:grid-cols-6 lg:grid-cols-8 gap-x-1 gap-y-5 md:gap-x-5 lg:px-0 lg:mx-1 lg:pt-4 lg:gap-y-3">
+          {herbCollectionList.map((info, idx) => {
+            return <MainHerbAvatarInfo info={info} key={idx} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Collections;
