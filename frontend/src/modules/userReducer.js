2670ef60deb0b029be1f855473de6640176ee03e
1upright <kompany11@hanmail.net>
2022-08-15 05:30:11 +0900

    [S07P12A604-266] feat: set user in state

--- frontend/src/modules/userReducer.js
+++ frontend/src/modules/userReducer.js
@@ -1,24 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  isLogin: false,
-  token: null,
-  userId: null,
+    isLogin: false,
+    token: null,
+    userId: null,
+    user: {
+        id: null,
+        name: null,
+        regDate: null,
+        description: null,
+        profileImgUrl: null,
+        dday: null,
+        currentSley: 0,
+        currentBookPoint: 0,
+    }
+    // user: null,
 };
 
 const reducers = {
-  login: (state) => {
-    state.isLogin = true;
-  },
-  logout: (state) => {
-    state.isLogin = false;
-  },
-  setToken: (state, { payload: token }) => {
-    state.token = token;
-  },
-  setUserId: (state, { payload: id }) => {
-    state.userId = id;
-  },
+    login: (state) => {
+        state.isLogin = true;
+    },
+    logout: (state) => {
+        state.isLogin = false;
+    },
+    setToken: (state, { payload: token }) => {
+        state.token = token;
+    },
+    setUserId: (state, { payload: id }) => {
+        state.userId = id;
+    },
+    setUser: (state, { payload: user }) => {
+        state.user = user;
+    },
 };
 
 export const userSlice = createSlice({
@@ -27,6 +41,6 @@ export const userSlice = createSlice({
   reducers,
 });
 
-export const { login, logout, setToken, setUserId } = userSlice.actions;
+export const { login, logout, setToken, setUserId, setUser } = userSlice.actions;
 
 export default userSlice.reducer;
