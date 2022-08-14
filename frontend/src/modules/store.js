a0c52c8271fc1568c19749f008923db70f939a63
1upright <kompany11@hanmail.net>
2022-08-15 05:25:12 +0900

    [S07P12A604-266] fix: seperate user and auth

--- frontend/src/modules/store.js
+++ frontend/src/modules/store.js
@@ -3,13 +3,15 @@ import persistReducer from "redux-persist/es/persistReducer";
 // import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import storage from "redux-persist/lib/storage";
 import { roomApi } from "../services/room";
+import { authApi } from "../services/auth";
 import { userApi } from "../services/user";
 import userReducer from "./userReducer";
 
 const reducers = combineReducers({
-  user: userReducer,
-  [roomApi.reducerPath]: roomApi.reducer,
-  [userApi.reducerPath]: userApi.reducer,
+    user: userReducer,
+    [roomApi.reducerPath]: roomApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
 });
 
 const persistedReducer = persistReducer(
@@ -21,12 +23,13 @@ const persistedReducer = persistReducer(
 );
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ serializableCheck: false }).concat([
-      roomApi.middleware,
-      userApi.middleware,
-    ]),
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({ serializableCheck: false }).concat([
+            roomApi.middleware,
+            authApi.middleware,
+            userApi.middleware,
+        ]),
 });
 
 // setupListeners(store.dispatch);
