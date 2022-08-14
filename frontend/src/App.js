a58427509cd5bb0294f6433d2a8d32911220173e
yoonsunny17 <jyoonsun0217@gmail.com>
2022-08-14 15:22:24 +0900

    [S07P12A604-298] fix: studyroom router link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,32 +6,33 @@ import StudySession from "./OpenVidu/StudySession";
 import MyPage from "./UI/pages/MyPage";
 import NotFound from "./UI/pages/NotFound";
 import KakaoLogin from "./UI/pages/KakaoLogin";
+import EnterStudyRoom from "./UI/pages/EnterStudyRoom";
+import SearchStudyRooms from "./UI/pages/SearchStudyRooms";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Drawer>
-                <div className="App container mx-auto px-5">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route
-                            path="/room/create"
-                            element={<CreateStudyRoom />}
-                        />
-                        <Route path="/login" element={<KakaoLogin />} />
-                        {/* <Route path="/studyroom" element={<StudyRoom />} /> */}
-                        {/* <Route
+  return (
+    <BrowserRouter>
+      <Drawer>
+        <div className="App container mx-auto px-5">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/room/create" element={<CreateStudyRoom />} />
+            <Route path="/login" element={<KakaoLogin />} />
+            {/* <Route path="/studyroom" element={<StudyRoom />} /> */}
+            {/* <Route
                             path="/studyroom"
                             element={<FingerStudyRoom />}
                         /> */}
-                        <Route path="/room" element={<StudySession />} />
-                        <Route path="/me" element={<MyPage />} />
-                        <Route path="*" element={<NotFound />} />
-                    </Routes>
-                </div>
-            </Drawer>
-        </BrowserRouter>
-    );
+            <Route path="/room/detail" element={<StudySession />} />
+            {/* <Route path="/room" element={<EnterStudyRoom />} /> */}
+            <Route path="/me" element={<MyPage />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/room" element={<SearchStudyRooms />} />
+          </Routes>
+        </div>
+      </Drawer>
+    </BrowserRouter>
+  );
 }
 
 export default App;
