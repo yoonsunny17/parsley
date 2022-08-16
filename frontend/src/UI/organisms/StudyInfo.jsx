7d7d3c77f49d4326393a1aea881d031137e4c198
KANCHOEUN <kancho1216@naver.com>
2022-08-16 16:33:33 +0900

    [S07P12A604-267] refactor: common toast code

--- frontend/src/UI/organisms/StudyInfo.jsx
+++ frontend/src/UI/organisms/StudyInfo.jsx
@@ -256,13 +256,4 @@ function StudyInfo() {
   );
 }
 
-const Toast = Swal.mixin({
-    toast: true,
-    width: 320,
-    position: "top-right",
-    showConfirmButton: false,
-    timer: 1500,
-    timerProgressBar: true,
-});
-
 export default StudyInfo;
