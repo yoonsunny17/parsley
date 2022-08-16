c97b009873b22ce76fccf894bbc94a1693b531aa
KANCHOEUN <kancho1216@naver.com>
2022-08-16 19:44:00 +0900

    [S07P12A604-267] fix: undefined error

--- frontend/src/modules/roomReducer.js
+++ frontend/src/modules/roomReducer.js
@@ -1,29 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// room: {
+//             id: null,
+//         hostUser: null,
+//         members: [],
+//         name: null,
+//         imageUrl: null,
+//         mode: null,
+//         description: null,
+//         maxPopulation: null,
+//         isPublic: null,
+//         hashtags: [],
+// }
+
 const initialState = {
-  room: {
-    id: null,
-    hostUser: null,
-    name: null,
-    imgUrl: null,
-    mode: null,
-    description: null,
-    maxPopulation: null,
-    isPublic: null,
-    members: [],
-  },
+    room: null,
 };
 
 const reducers = {
-  setRoom: (state, { payload: room }) => {
-    state.room = room;
-  },
+    setRoom: (state, { payload: room }) => {
+        state.room = room;
+    },
 };
 
 export const roomSlice = createSlice({
-  name: "room",
-  initialState,
-  reducers,
+    name: "room",
+    initialState,
+    reducers,
 });
 
 export const { setRoom } = roomSlice.actions;
