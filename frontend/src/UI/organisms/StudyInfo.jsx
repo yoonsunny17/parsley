b2dfda9df0b6ec88ac5ac60b9d679cc5c6770dba
KANCHOEUN <kancho1216@naver.com>
2022-08-16 17:06:42 +0900

    [S07P12A604-267] fix: render error

--- frontend/src/UI/organisms/StudyInfo.jsx
+++ frontend/src/UI/organisms/StudyInfo.jsx
@@ -4,8 +4,8 @@ import { Link, Navigate, useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { useGetRoomQuery } from "../../services/room";
 import {
-  useJoinRoomMutation,
-  useWithdrawRoomMutation,
+    useJoinRoomMutation,
+    useWithdrawRoomMutation,
 } from "../../services/userRoom";
 import Button from "../atoms/Button";
 import Swal from "sweetalert2";
@@ -100,144 +100,160 @@ function StudyInfo() {
     });
   };
 
-  return (
-    <div className="container flex flex-wrap gap-10">
-      {/* 스터디 이미지 */}
-      <div className="w-full lg:w-[30%] flex justify-center">
-        <div className="w-[20%] p-[20%] lg:w-full aspect-square overflow-hidden relative rounded-2xl shadow border-4 border-sub1">
-          <img
-            alt="스터디 이미지"
-            src={room?.imageUrl}
-            className="absolute top-0 left-0 w-full h-full object-cover"
-          />
-        </div>
-      </div>
-      {/* 스터디 상세 정보 */}
-      <div className="flex flex-col justify-between gap-4 min-h-min w-full px-2 lg:w-7/12 lg:px-0">
-        {/* Header: 이름 + 태그 */}
-        <div className="flex flex-col gap-4">
-          <div>
-            <div className="text-2xl font-semibold">{room?.name}</div>
-            {room?.hashtags.length > 0 && (
-              <div className="flex gap-2 mt-2">
-                {room?.hashtags.map((hashtag, idx) => {
-                  return (
-                    <span
-                      key={`tag-${idx}`}
-                      className="text-sm bg-sub1 text-font3 rounded-[10px] px-2 py-1"
-                    >
-                      {`# ${hashtag}`}
-                    </span>
-                  );
-                })}
-              </div>
-            )}
-          </div>
-          {/* Content: 상세 정보 */}
-          <div className="flex flex-col gap-2">
-            <div className="text-sm flex">
-              <div className="mr-4 text-extra3">방장</div>
-              <div className="font-light text-extra5">
-                {room?.hostUser.name}
-              </div>
-            </div>
-            <div className="text-sm flex">
-              <div className="mr-4 min-w-max text-extra3">인원</div>
-              <span className="font-light text-extra5">
-                {room?.members.length} / {room?.maxPopulation} 명
-              </span>
-            </div>
-            <div className="text-sm flex">
-              <div className="mr-4 min-w-max text-extra3">소개</div>
-              <div className="text-sm font-light text-extra5">
-                {room?.description}
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* Footer: 참가 버튼 */}
-        <div className="flex items-center gap-4">
-          {/* 호스트인 경우 */}
-          {isLogin && user?.id === room?.hostUser.id ? (
-            <div className="flex gap-2">
-              {/* 지금 바로 공부하러 가는거 */}
-              <Link to={`/room/session/${params.id}`}>
-                <Button text={"입장하기"} />
-              </Link>
-              {/* // FIXME: 설정 버튼 다른 디자인으로 바꿀것! */}
-              <button className="color-delay rounded-full text-sm bg-main hover:bg-sub2 text-font3">
-                <label
-                  htmlFor="editSession"
-                  className="cursor-pointer px-4 py-2"
-                >
-                  수정하기
-                </label>
-              </button>
-              <input
-                type="checkbox"
-                id="editSession"
-                className="modal-toggle"
-              />
-              <div className="modal">
-                <div className="modal-box">
-                  <div className="flex justify-between">
-                    <div className="text-lg font-bold mb-3">방 정보 수정</div>
-                    <button>
-                      <label
-                        htmlFor="editSession"
-                        className="cursor-pointer px-2 py-2"
-                      >
-                        닫기
-                      </label>
-                    </button>
-                  </div>
-
-                  <div>방 제목</div>
-                  <div>최대 인원</div>
-                  <div>소개</div>
-                  <div>비공개/공개 여부: 비번도 바꿀수있나?</div>
-                  <div className="modal-action">
-                    {/* // TODO: 적용하기, 취소, 닫기 버튼 만들기 */}
-                    <button className="cursor-pointer px-2 py-2">
-                      수정하기
-                    </button>
-                    <button className="cursor-pointer px-2 py-2">
-                      방 삭제하기
-                    </button>
-                  </div>
+    return (
+        <div className="container flex flex-wrap gap-10">
+            {/* 스터디 이미지 */}
+            <div className="w-full lg:w-[30%] flex justify-center">
+                <div className="w-[20%] p-[20%] lg:w-full aspect-square overflow-hidden relative rounded-2xl shadow border-4 border-sub1">
+                    <img
+                        alt="스터디 이미지"
+                        src={room?.imageUrl}
+                        className="absolute top-0 left-0 w-full h-full object-cover"
+                    />
                 </div>
-              </div>
             </div>
-          ) : (
-            <div className="flex gap-2">
-              {/* 한번도 참여한 적 없는 경우 "추가하기" 버튼 활성화   */}
-              {room?.members.filter((member) => member.id === user?.id)
-                .length === 0 && (
-                <Button text={"추가하기"} onClick={handleJoinRoom} />
-              )}
+            {/* 스터디 상세 정보 */}
+            <div className="flex flex-col justify-between gap-4 min-h-min w-full px-2 lg:w-7/12 lg:px-0">
+                {/* Header: 이름 + 태그 */}
+                <div className="flex flex-col gap-4">
+                    <div>
+                        <div className="text-2xl font-semibold">
+                            {room?.name}
+                        </div>
+                        {room?.hashtags.length > 0 && (
+                            <div className="flex gap-2 mt-2">
+                                {room?.hashtags.map((hashtag, idx) => {
+                                    return (
+                                        <span
+                                            key={`tag-${idx}`}
+                                            className="text-sm bg-sub1 text-font3 rounded-[10px] px-2 py-1"
+                                        >
+                                            {`# ${hashtag}`}
+                                        </span>
+                                    );
+                                })}
+                            </div>
+                        )}
+                    </div>
+                    {/* Content: 상세 정보 */}
+                    <div className="flex flex-col gap-2">
+                        <div className="text-sm flex">
+                            <div className="mr-4 text-extra3">방장</div>
+                            <div className="font-light text-extra5">
+                                {room?.hostUser.name}
+                            </div>
+                        </div>
+                        <div className="text-sm flex">
+                            <div className="mr-4 min-w-max text-extra3">
+                                인원
+                            </div>
+                            <span className="font-light text-extra5">
+                                {room?.members.length} / {room?.maxPopulation}{" "}
+                                명
+                            </span>
+                        </div>
+                        <div className="text-sm flex">
+                            <div className="mr-4 min-w-max text-extra3">
+                                소개
+                            </div>
+                            <div className="text-sm font-light text-extra5">
+                                {room?.description}
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-              {/* 한번이라도 참여한 적 있는 경우 "참가하기" 버튼 활성화 */}
-              {room?.members.filter((member) => member.id === user?.id).length >
-                0.length > 0 && (
-                <>
-                  <Link to={`/room/session/${params.id}`}>
-                    <Button text={"입장하기"} />
-                  </Link>
-                  {/* // FIXME: 탈퇴하기 버튼 다른 디자인으로 바꿀것! */}
-                  <Button
+                {/* Footer: 참가 버튼 */}
+                <div className="flex items-center gap-4">
+                    {/* 호스트인 경우 */}
+                    {isLogin && user?.id === room?.hostUser.id ? (
+                        <div className="flex gap-2">
+                            {/* 지금 바로 공부하러 가는거 */}
+                            <Link to={`/room/session/${params.id}`}>
+                                <Button text={"입장하기"} />
+                            </Link>
+                            {/* // FIXME: 설정 버튼 다른 디자인으로 바꿀것! */}
+                            <button className="color-delay rounded-full text-sm bg-main hover:bg-sub2 text-font3">
+                                <label
+                                    htmlFor="editSession"
+                                    className="cursor-pointer px-4 py-2"
+                                >
+                                    수정하기
+                                </label>
+                            </button>
+                            <input
+                                type="checkbox"
+                                id="editSession"
+                                className="modal-toggle"
+                            />
+                            <div className="modal">
+                                <div className="modal-box">
+                                    <div className="flex justify-between">
+                                        <div className="text-lg font-bold mb-3">
+                                            방 정보 수정
+                                        </div>
+                                        <button>
+                                            <label
+                                                htmlFor="editSession"
+                                                className="cursor-pointer px-2 py-2"
+                                            >
+                                                닫기
+                                            </label>
+                                        </button>
+                                    </div>
+
+                                    <div>방 제목</div>
+                                    <div>최대 인원</div>
+                                    <div>소개</div>
+                                    <div>
+                                        비공개/공개 여부: 비번도 바꿀수있나?
+                                    </div>
+                                    <div className="modal-action">
+                                        {/* // TODO: 적용하기, 취소, 닫기 버튼 만들기 */}
+                                        <button className="cursor-pointer px-2 py-2">
+                                            수정하기
+                                        </button>
+                                        <button className="cursor-pointer px-2 py-2">
+                                            방 삭제하기
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="flex gap-2">
+                            {/* 한번도 참여한 적 없는 경우 "추가하기" 버튼 활성화   */}
+                            {room?.members.filter(
+                                (member) => member.id === user?.id
+                            ).length === 0 && (
+                                <Button
+                                    text={"추가하기"}
+                                    onClick={handleJoinRoom}
+                                />
+                            )}
+
+                            {/* 한번이라도 참여한 적 있는 경우 "참가하기" 버튼 활성화 */}
+                            {room?.members.filter(
+                                (member) => member.id === user?.id
+                            ).length > 0 && (
+                                <>
+                                    <Link to={`/room/session/${params.id}`}>
+                                        <Button text={"입장하기"} />
+                                    </Link>
+                                    {/* // FIXME: 탈퇴하기 버튼 다른 디자인으로 바꿀것! */}
+                                    <Button
                                         text={"탈퇴하기"}
                                         onClick={handleWithdrawRoom}
-                                    onClick={handleWithdrawRoom} />
-                </>
-              )}
-            </div>
-          )}
+                                    />
+                                </>
+                            )}
+                        </div>
+                    )}
 
-                    {/* 클립보드 */}
+                              {/* 클립보드 */}
           <button onClick={copyLink} className="text-font1 rounded-[50px]">
-                        <i className="bx text-3xl bx-link"></i>
-                    </button>
+                                    <i className="bx text-3xl bx-link"></i>
+                              </button>
 
           {/* 좋아요 */}
           {!like && (
