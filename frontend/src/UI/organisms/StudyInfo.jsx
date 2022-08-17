471bb8f993eeb335a9e01690c32c8fdfc8fd0ead
yoonsun <jyoonsun0217@gmail.com>
2022-08-17 20:33:15 +0900

    [S07P12A604-298] feat: update and delete roomInfo

--- frontend/src/UI/organisms/StudyInfo.jsx
+++ frontend/src/UI/organisms/StudyInfo.jsx
@@ -13,6 +13,10 @@ import { Toast } from "../../util/common";
 import { setRoom } from "../../modules/roomReducer";
 import { useEffect } from "react";
 
+import { BiEditAlt } from "react-icons/bi";
+import { MdClose } from "react-icons/md";
+import { produceWithPatches } from "immer";
+
 function StudyInfo() {
   const navigate = useNavigate();
   const isLogin = useSelector((state) => state.user.isLogin);
@@ -99,69 +103,62 @@ function StudyInfo() {
       title: "링크가 복사되었습니다!",
     });
   };
-
-    return (
-        <div className="container flex flex-wrap gap-10">
-            {/* 스터디 이미지 */}
-            <div className="w-full lg:w-[30%] flex justify-center">
-                <div className="w-[20%] p-[20%] lg:w-full aspect-square overflow-hidden relative rounded-2xl shadow border-4 border-sub1">
-                    <img
-                        alt="스터디 이미지"
-                        src={room?.imageUrl}
-                        className="absolute top-0 left-0 w-full h-full object-cover"
-                    />
-                </div>
+  console.log(room);
+  return (
+    <div className="container flex flex-wrap gap-10">
+      {/* 스터디 이미지 */}
+      <div className="w-full lg:w-[30%] flex justify-center">
+        <div className="w-[20%] p-[20%] lg:w-full aspect-square overflow-hidden relative rounded-2xl shadow border-4 border-sub1">
+          <img
+            alt="스터디 이미지"
+            src={room?.imageUrl}
+            className="absolute top-0 left-0 w-full h-full object-cover"
+          />
+        </div>
+      </div>
+      {/* 스터디 상세 정보 */}
+      <div className="flex flex-col justify-between gap-4 min-h-min w-full px-2 lg:w-7/12 lg:px-0">
+        {/* Header: 이름 + 태그 */}
+        <div className="flex flex-col gap-4">
+          <div>
+            <div className="text-2xl font-semibold">{room?.name}</div>
+            {room?.hashtags.length > 0 && (
+              <div className="flex gap-2 mt-2">
+                {room?.hashtags.map((hashtag, idx) => {
+                  return (
+                    <span
+                      key={`tag-${idx}`}
+                      className="text-sm bg-sub1 text-font3 rounded-[10px] px-2 py-1"
+                    >
+                      {`# ${hashtag}`}
+                    </span>
+                  );
+                })}
+              </div>
+            )}
+          </div>
+          {/* Content: 상세 정보 */}
+          <div className="flex flex-col gap-2">
+            <div className="text-sm flex">
+              <div className="mr-4 text-extra3">방장</div>
+              <div className="font-light text-extra5">
+                {room?.hostUser.name}
+              </div>
             </div>
-            {/* 스터디 상세 정보 */}
-            <div className="flex flex-col justify-between gap-4 min-h-min w-full px-2 lg:w-7/12 lg:px-0">
-                {/* Header: 이름 + 태그 */}
-                <div className="flex flex-col gap-4">
-                    <div>
-                        <div className="text-2xl font-semibold">
-                            {room?.name}
-                        </div>
-                        {room?.hashtags.length > 0 && (
-                            <div className="flex gap-2 mt-2">
-                                {room?.hashtags.map((hashtag, idx) => {
-                                    return (
-                                        <span
-                                            key={`tag-${idx}`}
-                                            className="text-sm bg-sub1 text-font3 rounded-[10px] px-2 py-1"
-                                        >
-                                            {`# ${hashtag}`}
-                                        </span>
-                                    );
-                                })}
-                            </div>
-                        )}
-                    </div>
-                    {/* Content: 상세 정보 */}
-                    <div className="flex flex-col gap-2">
-                        <div className="text-sm flex">
-                            <div className="mr-4 text-extra3">방장</div>
-                            <div className="font-light text-extra5">
-                                {room?.hostUser.name}
-                            </div>
-                        </div>
-                        <div className="text-sm flex">
-                            <div className="mr-4 min-w-max text-extra3">
-                                인원
-                            </div>
-                            <span className="font-light text-extra5">
-                                {room?.members.length} / {room?.maxPopulation}{" "}
-                                명
-                            </span>
-                        </div>
-                        <div className="text-sm flex">
-                            <div className="mr-4 min-w-max text-extra3">
-                                소개
-                            </div>
-                            <div className="text-sm font-light text-extra5">
-                                {room?.description}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div className="text-sm flex">
+              <div className="mr-4 min-w-max text-extra3">인원</div>
+              <span className="font-light text-extra5">
+                {room?.members.length} / {room?.maxPopulation} 명
+              </span>
+            </div>
+            <div className="text-sm flex">
+              <div className="mr-4 min-w-max text-extra3">소개</div>
+              <div className="text-sm font-light text-extra5">
+                {room?.description}
+              </div>
+            </div>
+          </div>
+        </div>
 
         {/* Footer: 참가 버튼 */}
         <div className="flex items-center gap-4">
@@ -189,21 +186,32 @@ function StudyInfo() {
               <div className="modal">
                 <div className="modal-box">
                   <div className="flex justify-between">
-                    <div className="text-lg font-bold mb-3">방 정보 수정</div>
+                    <div className="text-lg font-bold mb-3">
+                      <span className="flex items-center">
+                        방 정보 수정
+                        <span className="ml-2 text-xl">
+                          <BiEditAlt />
+                        </span>
+                      </span>
+                    </div>
                     <button>
                       <label
                         htmlFor="editSession"
                         className="cursor-pointer px-2 py-2"
                       >
-                        닫기
+                        <MdClose />
                       </label>
                     </button>
                   </div>
+                  <div className="mt-3">
+                    <div>방 제목: {room?.name}</div>
+                    <div>최대 인원: {room?.maxPopulation}</div>
+                    <div>소개</div>
+                    <div>{room?.description}</div>
+                    <div>모드: {room?.mode === 0 ? "손꾸락" : "얼구리"}</div>
+                    <div>공개 여부: {room?.isPublic ? "공개" : "비공개"}</div>
+                  </div>
 
-                  <div>방 제목</div>
-                  <div>최대 인원</div>
-                  <div>소개</div>
-                  <div>비공개/공개 여부: 비번도 바꿀수있나?</div>
                   <div className="modal-action">
                     {/* // TODO: 적용하기, 취소, 닫기 버튼 만들기 */}
                     <button className="cursor-pointer px-2 py-2">
@@ -238,10 +246,10 @@ function StudyInfo() {
             </div>
           )}
 
-                              {/* 클립보드 */}
+          {/* 클립보드 */}
           <button onClick={copyLink} className="text-font1 rounded-[50px]">
-                                    <i className="bx text-3xl bx-link"></i>
-                              </button>
+            <i className="bx text-3xl bx-link"></i>
+          </button>
 
           {/* 좋아요 */}
           {!like && (
