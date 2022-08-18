4d71ab34f19598fc086bbbe901ed7d0e1f339c6f
yoonsun <jyoonsun0217@gmail.com>
2022-08-19 00:17:58 +0900

    [S07P12A604-136] feat: nonlogin main component and fix sidebar error

--- frontend/src/UI/organisms/Navbar.jsx
+++ frontend/src/UI/organisms/Navbar.jsx
@@ -2,61 +2,61 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import {
-    useGetNotificationCntQuery,
-    useReadNotificationMutation,
+  useGetNotificationCntQuery,
+  useReadNotificationMutation,
 } from "../../services/notification";
 import Button from "../atoms/Button";
 import Notification from "../molecules/Notification";
 import SearchBar from "../molecules/SearchBar";
 
 function Navbar() {
-    const isLogin = useSelector((state) => state.user.isLogin);
-    const { data } = useGetNotificationCntQuery(
-        {},
-        { skip: !isLogin, refetchOnMountOrArgChange: true }
-    );
-    const [readNotification] = useReadNotificationMutation();
-    const [notiArr, setNotiArr] = useState([]);
+  const isLogin = useSelector((state) => state.user.isLogin);
+  const { data } = useGetNotificationCntQuery(
+    {},
+    { skip: !isLogin, refetchOnMountOrArgChange: true }
+  );
+  const [readNotification] = useReadNotificationMutation();
+  const [notiArr, setNotiArr] = useState([]);
 
-    const clickAlarm = async () => {
-        const result = await readNotification().unwrap();
-        setNotiArr(result?.notifications);
-    };
+  const clickAlarm = async () => {
+    const result = await readNotification().unwrap();
+    setNotiArr(result?.notifications);
+  };
 
-    return (
-        <header className="h-[72px] flex justify-between items-center mt-0 mb-4">
-            <Link to="/">
-                <span className="logo font-logo text-3xl cursor-pointer">
-                    PARSLEY
-                </span>
+  return (
+    <header className="h-[72px] flex justify-between items-center mt-0 mb-4">
+      <Link to="/">
+        <span className="flex items-center">
+          <img className="w-11" src="/parsley_logo.png" alt="logo" />
+          <span className="logo font-logo text-3xl cursor-pointer">
+            PARSLEY
+          </span>
+        </span>
+      </Link>
+      <div className="flex items-center gap-[25px] relative">
+        {isLogin && (
+          <div className="hidden sm:flex sm:items-center sm:gap-[25px] sm:relative">
+            <Link to="/room/create">
+              <Button text={"스터디룸 생성"} color="primary" />
             </Link>
-            <div className="flex items-center gap-[25px] relative">
-                {isLogin && (
-                    <div className="hidden sm:flex sm:items-center sm:gap-[25px] sm:relative">
-                        <Link to="/room/create">
-                            <Button text={"스터디룸 생성"} color="primary" />
-                        </Link>
-                        {/* Notification */}
-                        <Notification
-                            onClick={clickAlarm}
-                            notiArr={notiArr}
-                            uncheckedCnt={data?.uncheckCnt}
-                        />
-                    </div>
-                )}
+            {/* Notification */}
+            <Notification
+              onClick={clickAlarm}
+              notiArr={notiArr}
+              uncheckedCnt={data?.uncheckCnt}
+            />
+          </div>
+        )}
 
-                {/* Search Bar */}
-                <SearchBar />
-                {/* Menu Button */}
-                <label
-                    htmlFor="my-drawer-4"
-                    className="drawer-button cursor-pointer"
-                >
-                    <i className="bx bx-menu text-4xl"></i>
-                </label>
-            </div>
-        </header>
-    );
+        {/* Search Bar */}
+        <SearchBar />
+        {/* Menu Button */}
+        <label htmlFor="my-drawer-4" className="drawer-button cursor-pointer">
+          <i className="bx bx-menu text-4xl"></i>
+        </label>
+      </div>
+    </header>
+  );
 }
 
 export default Navbar;
