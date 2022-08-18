6a06a30cd022de736b19ec4c0f7dbb19d933c52a
마유선 <jasminema@naver.com>
2022-08-19 00:13:14 +0900

    [S07P12A604-268] fix: update selected item effect

--- frontend/src/UI/atoms/HerbStoreItemAvatar.jsx
+++ frontend/src/UI/atoms/HerbStoreItemAvatar.jsx
@@ -1,12 +1,16 @@
 // 허브 상점에서 아이템 선택 라디오 버튼으로 보이는 아바타!
 import React from "react";
 
-function HerbStoreItemAvatar({ imgUrl, title, price }) {
+function HerbStoreItemAvatar({ imgUrl, title, price, checked }) {
   return (
     <>
       <div className="avatar mx-3 mt-4 mb-3 cursor-pointer">
         {/* <div className="w-16 rounded-xl"> */}
-        <div className="w-16 rounded-full ring-extra1 ring-offset-base-100 ring-2 hover:ring-sub1 color-delay focus:ring-sub1 active:ring-sub1">
+        <div
+          className={`w-16 rounded-full ring-offset-base-100 ring-2 hover:ring-sub1 color-delay ${
+            checked ? "ring-sub1" : "ring-extra1"
+          }`}
+        >
           <img src={imgUrl} alt="itemStore" />
         </div>
       </div>
