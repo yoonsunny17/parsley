e251dd66ebae584522c1d00706c183c2a9039d9c
KANCHOEUN <kancho1216@naver.com>
2022-08-20 21:59:27 +0900

    fix: public + password

--- frontend/src/services/room.js
+++ frontend/src/services/room.js
@@ -2,68 +2,76 @@ import { createApi } from "@reduxjs/toolkit/dist/query/react";
 import { baseQueryWithReAuth } from ".";
 
 export const roomApi = createApi({
-  reducerPath: "roomApi",
-  baseQuery: baseQueryWithReAuth,
-  endpoints: (builder) => ({
-    getAllRooms: builder.query({
-      query: () => `/room`,
+    reducerPath: "roomApi",
+    baseQuery: baseQueryWithReAuth,
+    endpoints: (builder) => ({
+        getAllRooms: builder.query({
+            query: () => `/room`,
+        }),
+        getRoom: builder.query({
+            query: (roomId) => `/room/${roomId}`,
+            providesTags: ["Room"],
+        }),
+        createRoom: builder.mutation({
+            query: (room) => {
+                return {
+                    url: `/room/create`,
+                    method: "POST",
+                    body: room,
+                };
+            },
+        }),
+        updateRoom: builder.mutation({
+            query: ({ id, ...rest }) => ({
+                url: `/room/${id}/update`,
+                method: "POST",
+                body: rest,
+            }),
+            invalidatesTags: ["Room"],
+        }),
+        checkPassword: builder.mutation({
+            query: ({ id, password }) => ({
+                url: `/room/${id}/check`,
+                method: "POST",
+                body: { password },
+            }),
+        }),
+        deleteRoom: builder.mutation({
+            query: (id) => ({
+                url: `/room/${id}`,
+                method: "POST",
+            }),
+        }),
+        getStudySession: builder.query({
+            query: (roomId) => `/room/session/${roomId}`,
+        }), // 스터디룸 참가
+        addStudyRooms: builder.query({
+            query: ({ ...rest }) => ({
+                url: `/room/add`,
+                method: "POST",
+                body: rest,
+            }),
+        }), // 스터디룸 추가 (한번도 참여 이력 없을 경우)
+        addStudyLog: builder.mutation({
+            query: ({ id, status }) => ({
+                url: `/room/${id}/log`,
+                method: "POST",
+                body: { status },
+            }),
+            async onQueryStarted(_, { dispatch, getState, queryFulfilled }) {
+                await queryFulfilled;
+            },
+        }),
     }),
-    getRoom: builder.query({
-      query: (roomId) => `/room/${roomId}`,
-      providesTags: ["Room"],
-    }),
-    createRoom: builder.mutation({
-      query: (room) => {
-        return {
-          url: `/room/create`,
-          method: "POST",
-          body: room,
-        };
-      },
-    }),
-    updateRoom: builder.mutation({
-      query: ({ id, ...rest }) => ({
-        url: `/room/${id}/update`,
-        method: "POST",
-        body: rest,
-      }),
-      invalidatesTags: ["Room"],
-    }),
-    deleteRoom: builder.mutation({
-      query: (id) => ({
-        url: `/room/${id}`,
-        method: "POST",
-      }),
-    }),
-    getStudySession: builder.query({
-      query: (roomId) => `/room/session/${roomId}`,
-    }), // 스터디룸 참가
-    addStudyRooms: builder.query({
-      query: ({ ...rest }) => ({
-        url: `/room/add`,
-        method: "POST",
-        body: rest,
-      }),
-    }), // 스터디룸 추가 (한번도 참여 이력 없을 경우)
-    addStudyLog: builder.mutation({
-      query: ({ id, status }) => ({
-        url: `/room/${id}/log`,
-        method: "POST",
-        body: { status },
-      }),
-      async onQueryStarted(_, { dispatch, getState, queryFulfilled }) {
-        await queryFulfilled;
-      },
-    }),
-  }),
 });
 
 export const {
-  useGetAllRoomsQuery,
-  useGetRoomQuery,
-  useCreateRoomMutation,
-  useUpdateRoomMutation,
-  useDeleteRoomMutation,
-  useGetStudySessionQuery,
-  useAddStudyLogMutation,
+    useGetAllRoomsQuery,
+    useGetRoomQuery,
+    useCreateRoomMutation,
+    useUpdateRoomMutation,
+    useCheckPasswordMutation,
+    useDeleteRoomMutation,
+    useGetStudySessionQuery,
+    useAddStudyLogMutation,
 } = roomApi;
